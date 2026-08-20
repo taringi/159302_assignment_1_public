d0fcec3c2d3a3a0fdf586f486f98d987f8f08561
Alex Shkvoretz <alex@ss.house>
2026-08-20 23:58:27 +1000

    init commit

---
+++ algorithm.cpp
@@ -0,0 +1,89 @@
+
+
+#include "algorithm.h"
+
+using namespace std;
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+// Search Algorithm:  UC with Strict Expanded List
+//
+// Move Generator:  
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+string uc_explist(string const initialState, string const goalState, int& pathLength, int &numOfStateExpansions, int& maxQLength,
+                               float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap, int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions){
+											 
+   string path;
+   clock_t startTime;
+   
+   numOfDeletionsFromMiddleOfHeap=0;
+   numOfLocalLoopsAvoided=0;
+   numOfAttemptedNodeReExpansions=0;
+
+
+    // cout << "------------------------------" << endl;
+    // cout << "<<uc_explist>>" << endl;
+    // cout << "------------------------------" << endl;
+	actualRunningTime=0.0;	
+	startTime = clock();
+	srand(time(NULL)); //RANDOM NUMBER GENERATOR - ONLY FOR THIS DEMO.  YOU REALLY DON'T NEED THIS! DISABLE THIS STATEMENT.
+	maxQLength= rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY.
+	numOfStateExpansions = rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY
+
+
+	
+	
+//***********************************************************************************************************
+	actualRunningTime = ((float)(clock() - startTime)/CLOCKS_PER_SEC);
+	path = "DDRRLLLUUURDLUDURDLUU"; //this is just a dummy path for testing the function
+	pathLength = path.size();
+	return path;		
+		
+}
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+// Search Algorithm:  A* with the Strict Expanded List
+//
+// Move Generator:  
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+string aStar_ExpandedList(string const initialState, string const goalState, int& pathLength, int &numOfStateExpansions, int& maxQLength,
+                               float &actualRunningTime, int &numOfDeletionsFromMiddleOfHeap, int &numOfLocalLoopsAvoided, int &numOfAttemptedNodeReExpansions, heuristicFunction heuristic){
+											 
+   string path;
+   clock_t startTime;
+   
+   numOfDeletionsFromMiddleOfHeap=0;
+   numOfLocalLoopsAvoided=0;
+   numOfAttemptedNodeReExpansions=0;
+
+
+    // cout << "------------------------------" << endl;
+    // cout << "<<aStar_ExpandedList>>" << endl;
+    // cout << "------------------------------" << endl;
+	actualRunningTime=0.0;	
+	startTime = clock();
+	srand(time(NULL)); //RANDOM NUMBER GENERATOR - ONLY FOR THIS DEMO.  YOU REALLY DON'T NEED THIS! DISABLE THIS STATEMENT.
+	maxQLength= rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY.
+	numOfStateExpansions = rand() % 200; //AT THE MOMENT, THIS IS JUST GENERATING SOME DUMMY VALUE.  YOUR ALGORITHM IMPLEMENTATION SHOULD COMPUTE THIS PROPERLY
+
+
+	
+	
+//***********************************************************************************************************
+	actualRunningTime = ((float)(clock() - startTime)/CLOCKS_PER_SEC);
+	path = "DDRRLLLUUURDLUDURDLUU"; //this is just a dummy path for testing the function
+	pathLength = path.size();
+	return path;		
+		
+}
+
---
+++ makefile
@@ -0,0 +1,53 @@
+CC := g++
+TARGET := search
+
+# Detect the operating system
+ifeq ($(OS),Windows_NT)
+	# Windows
+	CFLAGS := -O2 -std=c++11 -Wall -c
+    LFLAGS := -lgdi32
+#     LFLAGS :=
+    EXTENSION := .exe
+	CLEANUP := del
+	CLEANUP_OBJS := del *.o
+# 	CLEANUP_OBJS := del *.exe
+else
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Darwin)
+		# macOS
+		EXTENSION := .out
+		CFLAGS := -O2 -std=c++11 -Wall -I/usr/local/include -L/usr/local/lib -c
+		LFLAGS := -lSDL_bgi -lSDL2 
+		CLEANUP := rm -f
+		CLEANUP_OBJS := rm -f *.o
+	else ifeq ($(UNAME_S),Linux)
+		# Linux
+		EXTENSION := .out
+		CFLAGS := -O2 -std=c++11 -Wall -I/usr/local/include -L/usr/local/lib -c   
+		LFLAGS := -lSDL_bgi -lSDL2 
+		CLEANUP := rm -f
+		CLEANUP_OBJS := rm -f *.o
+	endif
+endif
+
+# Find all source files (.cpp) and header files (.h)
+SRCS := $(wildcard *.cpp) $(wildcard */*.cpp)
+HDRS := $(wildcard *.h) $(wildcard */*.h)
+
+# Create object file names based on source file names
+OBJS := $(SRCS:.cpp=.o)
+
+# Output executable
+# EXECUTABLE := search.exe
+
+# Rule to build the executable
+$(TARGET)$(EXTENSION): $(OBJS)
+	$(CC) -O2 -Wl,-s -o $@ $(OBJS) $(LFLAGS)
+
+# Rule to build object files
+%.o: %.cpp $(HDRS)
+	$(CC) -O2 -std=c++11 -c $< -o $@
+
+clean:
+	$(CLEANUP) $(TARGET)$(EXTENSION)
+	$(CLEANUP_OBJS)
